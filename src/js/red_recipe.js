7ab660cff69cb387e058531b5f37d85c5d01f6c8
JxyStar <17600265291@163.com>
2017-08-24 00:21:11 +0800

    修改主页子页面

---
+++ src/js/red_recipe.js
@@ -0,0 +1,8 @@
+/**
+ * Created by 梨涡浅笑 on 2017/8/23.
+ */
+$(function(){
+    $('.h-left').click(function(){
+        window.location.href = '../html/scheme.html';
+    })
+})
--- src/js/scheme.js
+++ src/js/scheme.js
@@ -5,21 +5,34 @@
 * @Last Modified time: 2017-08-23 09:41:00
 */
 
-    var mySwiper = new Swiper ('.main_banner', {
-            slidesPerView: 'auto',
-            paginationClickable: true,
-            spaceBetween:25
-        })
-        var myBanner = new Swiper ('.banner', {
-            // 如果需要分页器
-            pagination: '.swiper-pagination',
-            autoplay:5000
-        })
-        $(function(){
-            $('.btn_href').click(function(){
-                window.location.href='add_target.html';
-            })
-            $('.main_img').children().eq(0).click(function(){
-                window.location.href='recipe.html';
-            })
-        })
+var mySwiper = new Swiper ('.main_banner', {
+        slidesPerView: 'auto',
+        paginationClickable: true,
+        spaceBetween:25
+    })
+    var myBanner = new Swiper ('.banner', {
+        // 如果需要分页器
+        pagination: '.swiper-pagination',
+        autoplay:5000
+    })
+$(function(){
+    $('.btn_href').click(function(){
+        window.location.href='add_target.html';
+    })
+    $('.my_width').children().eq(0).click(function(){
+        window.location.href='../html/recipe.html';
+    })
+    $('.my_width').children().eq(1).click(function(){
+        window.location.href='../html/red_recipe.html';
+    })
+    $('.my_width').children().eq(2).click(function(){
+        window.location.href='../html/orange_recipe.html';
+    })
+    $('.imgs').on('click','li:eq(0)',function(){
+        window.location.href='../html/recipe.html';
+    }).on('click','li:eq(1)',function(){
+        window.location.href='../html/red_recipe.html';
+    }).on('click','li:eq(2)',function(){
+        window.location.href='../html/orange_recipe.html';
+    })
+})
