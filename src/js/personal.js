0530bdd9fad2512881c3e8070cd577cac1a76d1c
DaiMaXinXuan <15210971852@163.com>
2017-08-23 15:31:56 +0800

    修改注册昵称

--- src/js/personal.js
+++ src/js/personal.js
@@ -2,6 +2,9 @@ $(function(){
 	$('.h-left').click(function(){
 		window.history.go(-1);
 	})
+	$('.log-off_btn').click(function(){
+		window.location.href = "register.html";
+	})
     var name_data=localStorage.getItem('name');
     $('#name').html(JSON.parse(name_data));
 })
